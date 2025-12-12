8b382c89a4504547fcc2d60799c70add63089cc2
augustin <augustinkongupudi5@gmail.com>
2025-12-12 03:06:17 -0800

    create login page

--- my-vite-app/src/Components/Loginpage.jsx
+++ my-vite-app/src/Components/Loginpage.jsx
@@ -1,24 +1,31 @@
 import "../Css/Login.css"
 
-
+import bgImage from "../assets/lodo.jpeg";
 
 function Loginpage(){
     return(
-        <div className="Login">
-        <h1>Electro_Mart</h1>
-        <div className="box">
-            <h2>Log in to Electrolmart</h2>
-            <label htmlFor="">
-                username:
-                <input type="text" />
-            </label><br />
-            <label htmlFor="">
-                password :
-                <input type="text" />
-            </label><br /><br />
-            <button>Login</button>
-        </div>
-        </div>
+         <div class="container">
+            
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="user name" required />
+                    <i class='bx bxs-user-circle'></i>
+                </div>
+                <div class="input-box">
+                    <input type="text" placeholder="password" required />
+                    
+                    <i class='bx bx-lock'></i>
+                </div>
+
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Remember me</label>
+                    <a href="#">Forgot password?</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div className="register-link">
+                    <p>Don't have account ? <a href="#">Register</a></p>
+                </div>
+            </div>
     );
 }
 export default Loginpage;
