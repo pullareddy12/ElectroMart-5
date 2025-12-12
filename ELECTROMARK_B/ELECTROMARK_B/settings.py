da5877fa96cd2c510b8f4aa3554d44768812b5cb
Pullareddy <pullareddyanakala7@gmail.com>
2025-12-12 16:05:00 +0530

    updatred -.env file updated -.gitignore updated

--- ELECTROMARK_B/ELECTROMARK_B/settings.py
+++ ELECTROMARK_B/ELECTROMARK_B/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()  # reads .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,12 +74,13 @@ WSGI_APPLICATION = 'ELECTROMARK_B.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ElectroMart',
-        'USER': 'root',                   # Your MySQL username
-        'PASSWORD': 'Pullareddy@628',     # Your MySQL password
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),                   # Your MySQL username
+        'PASSWORD': os.getenv('PASSWORD'),    # Your MySQL password
         'HOST': 'localhost',              # Or '127.0.0.1'
         'PORT': '3306',                   # Default MySQL port
         
